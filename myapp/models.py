e70b64d506e5984919bf0d7860fcfa9bf37fd93a
Ceasar15 <ceasarkwadwo@gmail.com>
2020-08-27 01:34:31 +0100

    fixed databases

--- myapp/models.py
+++ myapp/models.py
@@ -6,23 +6,23 @@ from django.db import models
 
 class Contact(models.Model):
     fullname = models.CharField(null=True, max_length=100, blank=True)
-    email = models.EmailField(default="kwadwo123@gmail.com", max_length=30, blank=True)
-    phone = models.IntegerField(null=True, blank=False)
-    message = models.TextField(blank=False)
+    email = models.EmailField(default="kwadwo123@gmail.com", max_length=100, blank=True)
+    phone = models.IntegerField(max_length=100, null=True, blank=False)
+    message = models.TextField(max_length=300, blank=False)
 
     def __str__(self):
         return str(self.fullname)
 
 
 class People(models.Model):
-    fullname = models.CharField(max_length=50, null=False, blank=False)
-    phone = models.IntegerField(null=False, blank=False)
-    hall = models.CharField(max_length=50, null=False, blank=False)
-    roomNumber = models.CharField(max_length=15, blank=True)
+    fullname = models.CharField(max_length=100, null=False, blank=False)
+    phone = models.IntegerField(max_length=100, null=False, blank=False)
+    hall = models.CharField(max_length=100, null=False, blank=False)
+    roomNumber = models.CharField(max_length=100, blank=True)
     program = models.CharField(max_length=50, null=False, blank=False)
-    birthday = models.DateField(blank=False)
-    gender = models.CharField(null=True, max_length=15)
-    level = models.CharField(max_length=10)
+    birthday = models.DateField(max_length=100,blank=False)
+    gender = models.CharField(null=True, max_length=100)
+    level = models.CharField(max_length=100)
 
     def __str__(self):
         return str(People)
